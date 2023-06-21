e7c46f3e34ded2ec24cc9fc563f7d496cb0d9b50
emmahewson <emhewson@hotmail.com>
2023-06-21 11:19:14 +0000

    Added checkout forms, admin & signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+# allows admin to edit the Order line item from the table
+# rather than clicking through to the order lineitem admin page
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+# Don't need to register OrderLineItem as it's accessible
+# on the inline of the OrderAdmin
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -30,12 +30,12 @@ class Order(models.Model):
         null=False,
         blank=False
     )
-    country = models.EmailField(
+    country = models.CharField(
         max_length=40,
         null=False,
         blank=False
     )
-    postcode = models.EmailField(
+    postcode = models.CharField(
         max_length=20,
         null=True,
         blank=True
