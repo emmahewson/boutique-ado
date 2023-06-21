14194719bf9bb841124fc42c355b793ead0177b2
emmahewson <emhewson@hotmail.com>
2023-06-21 15:51:45 +0000

    Added order summary to checkout success

--- checkout/views.py
+++ checkout/views.py
@@ -108,8 +108,8 @@ def checkout_success(request, order_number):
     save_info = request.session.get("save_info")
     order = get_object_or_404(Order, order_number=order_number)
     messages.success(request, (
-        f'Order sucesssfully processed!'
-        f'Your order number is {order_number}.'
+        f'Order sucesssfully processed! '
+        f'Your order number is {order_number}. '
         f'A confirmation email will be sent to {order.email}.'
         )
     )
