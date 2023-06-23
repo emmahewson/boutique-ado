15196544899c0ab87ab752074779a8f59d4804e8
emmahewson <emhewson@hotmail.com>
2023-06-23 11:03:55 +0000

    Added edit product template, view & url

--- products/views.py
+++ products/views.py
@@ -96,3 +96,31 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'The product was updated successfully!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, (
+                f'Failed to update product, please check your form for errors.'))
+    else:
+        form = ProductForm(instance=product)
+
+    messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product
+    }
+
+    return render(request, template, context)
