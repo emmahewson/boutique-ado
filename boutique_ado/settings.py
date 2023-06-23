0e36f6d2b1fb966c11e70aa3d6516c353d7e62d9
emmahewson <emhewson@hotmail.com>
2023-06-23 14:20:23 +0000

    Replaced secret key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,13 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-1^t!pok1ejftv2o4l6t36'
-    '-=d6+p=_12x2ph9=gp=$$gdmntl5)'
-)
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-emmahewson-boutiqueado-bae3ut9ju24.ws-eu99.gitpod.io',
