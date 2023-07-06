d984f658eeffb4951ee3c2748796b1b4d66da504
emmahewson <emhewson@hotmail.com>
2023-07-06 10:25:23 +0000

    Sending real emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,6 +30,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = [
     '8000-emmahewson-boutiqueado-bae3ut9ju24.ws-eu99.gitpod.io',
     '8000-emmahewson-boutiqueado-bae3ut9ju24.ws-eu100.gitpod.io',
+    '8000-emmahewson-boutiqueado-bae3ut9ju24.ws-eu101.gitpod.io',
     'boutique-ado-eh.herokuapp.com',
     'boutique-ado-eh-80eb0c547dca.herokuapp.com',
     'localhost',
@@ -112,8 +113,6 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -232,4 +231,15 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
